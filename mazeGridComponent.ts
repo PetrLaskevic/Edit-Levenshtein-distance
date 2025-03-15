01865c7be4c5a882368d31aa89aff2d4221c59af
PetrLaskevic <139137582+PetrLaskevic@users.noreply.github.com>
2025-03-15 21:16:33 +0100

    (grid) Define a TS interface

--- mazeGridComponent.ts
+++ mazeGridComponent.ts
@@ -1,8 +1,14 @@
 interface GridInterface{
-    rows: number,
-    maximumTextLengthSetSoFar: number
+    rows: number;
+    columns: number;
+    cellStyles: string;
+    cellSize: number;
+    maximumTextLengthSetSoFar: number;
+    resizeCallback: (this: Window, ev: UIEvent) => void;
+    maximumTextLengthElement?: HTMLDivElement;
+    grid: HTMLDivElement;
 }
-class ResponsiveGrid extends HTMLElement { // implements GridInterface
+class ResponsiveGrid extends HTMLElement implements GridInterface{ // 
     //when the element is initialised like this, the rows & columns aren't set in the constructor
     // let grid = document.createElement("responsive-grid");
     // grid.rows = this.numberOfRows;
